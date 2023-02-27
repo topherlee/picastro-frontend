b415e04700b7e233299b3acef501314c2f0171dd
nutcracker22 <nutcracker@mailbox.org>
2023-02-27 22:32:28 +0000

    heavy refactoring of /components to make re-using them easier

--- components/organisms/index.js
+++ components/organisms/index.js
@@ -1,4 +1,2 @@
-export {default as HomeFeedContainer} from './HomeFeedContainer/HomeFeedContainer';
-export {default as DetailedFeedContainer} from './DetailedFeedContainer/DetailedFeedContainer';
 export {default as FullWidthPostsContainer} from './PostsContainers/FullWidthPostsContainer';
 export {default as HalfWidthPostsContainer} from './PostsContainers/HalfWidthPostsContainer';
