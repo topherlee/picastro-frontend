265ba93afb44a75b9929e6aae14dc1f6638bf547
Christopher Lee <99973434+topherlee@users.noreply.github.com>
2023-02-28 21:48:21 +0000

    Update index.js

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,8 +1,3 @@
-export {default as DetailedFeedImage} from './DetailedFeed/DetailedFeedImage';
-export {default as DetailedFeedBelowImage} from './DetailedFeed/DetailedFeedBelowImage';
-export {default as DetailedFeedAboveImage} from './DetailedFeed/DetailedFeedAboveImage';
-export {default as HomeFeedImageContainer} from './HomeFeedImageContainer/HomeFeedImageContainer';
-export {default as HomeFeedBelowImage} from './HomeFeedBelowImage/HomeFeedBelowImage';
 export {default as BottomFilterModal} from './Modals/BottomModal';
 export {default as PicastroLogoHeader} from './Headers/PicastroLogoHeader';
 export {default as UserNameImageBurgerHeader} from './Headers/UserNameImageBurgerHeader';
