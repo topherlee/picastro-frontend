5b291d8cf139f4449c66efb8b4890601057a0e0d
Christopher Lee <99973434+topherlee@users.noreply.github.com>
2023-03-02 01:06:51 +0000

    Add click to fullscreen zoom with modal (tested on ios)

--- src/components/organisms/PostsContainers/FullWidthPostsContainer.js
+++ src/components/organisms/PostsContainers/FullWidthPostsContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Button,
     Image,
@@ -10,25 +10,62 @@ import {
     Text,
     useColorScheme,
     View,
-    TouchableOpacity
+    TouchableOpacity,
+    Modal
 } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
 
 import styled from 'styled-components';
 import { FullWidthAboveImage, FullWidthImage, FullWidthBelowImage } from '../../molecules';
 
 export const FullWidthPostsContainer = ({props}) => {
-  console.log("DFC",props)
+  console.log("DFC",props);
+
+  const [modalVisible, setModalVisible] = useState(false);
   const navigation = useNavigation();
+  var source = Image.resolveAssetSource(props.imageURL);
+  ratio = (source.width / source.height);
     return (
         <View style={styles.container}>
+            <Modal
+                animationType="slide"
+                visible={modalVisible}
+                presentationStyle="pageSheet"
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >   
+                <ReactNativeZoomableView
+                    maxZoom={3}
+                    minZoom={1}
+                    zoomStep={0.5}
+                    initialZoom={1}
+                    bindToBorders={true}
+                    visualTouchFeedbackEnabled={false}
+                    onZoomAfter={this.logOutZoomState}
+                    disablePanOnInitialZoom={true}
+                    style={{backgroundColor: 'rgba(0, 0, 0, 0.95)'}}
+                >   
+                    <Image 
+                        source={props.imageURL}
+                        style={{
+                            aspectRatio: ratio,
+                            width: '100%',
+                            height: 'auto',
+                        }}
+                        resizeMode="contain"
+                    />
+                </ReactNativeZoomableView>
+            </Modal>
             <FullWidthAboveImage props={props} />
-            <View1>
+            <TouchableOpacity 
+                onPress={() => setModalVisible(true)}  >
                 <FullWidthImage
-                props={props}  
+                props={props}
                 />
-            </View1>
+            </TouchableOpacity>
             <FullWidthBelowImage props={props} />
         </View>
     )
