9736d81c7da14807967f202d70e894f0cd3c6031
nutcracker22 <nutcracker@mailbox.org>
2023-03-03 11:10:23 +0000

    changed API url for HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -269,7 +269,7 @@ const HomeScreen = ({ navigation }) => {
     const [data, setData] = useState([]);
 
     let getFeeds = () => {
-        fetch(`http://127.0.0.1:8000/api/feed/home`)
+        fetch(`http://10.0.2.2:8000/api/feed/home`)
             .then(res => {
                 console.log(res.status);
                 console.log(res.headers);
