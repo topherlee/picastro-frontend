2ccb8f1e82c2d8c1f110189c722303a76b8e89ff
nutcracker22 <nutcracker@mailbox.org>
2023-03-23 20:56:30 +0000

    introduced global styling for re-used styles

--- constants/globalStyling.js
+++ constants/globalStyling.js
@@ -35,5 +35,32 @@ export default StyleSheet.create({
         alignItems: 'center',
         gap: 5,
         top: 30
+    },
+    sortByModalSvg: {
+      color: 'blue',
+      width: 70,
+      height: 70
+    },
+    SortByModalText: {
+      fontFamily: 'Inter',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: 13,
+      textAlign: 'center'
+    },
+    SortByModalTextGrey: {
+      color: '#959595',
+    },
+    SortByModalTextYellow: {
+      color: '#FFC700'
+    },
+    iconContainer: {
+        width: 100,
+        height: 107,
+        borderColor: 'white',
+        borderWidth: 0,
+        alignItems: 'center',
+        gap: 5,
+        top: 30
     }
   });
