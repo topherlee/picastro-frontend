f89763dfa15dc0cde3edcd8c5be6b46bea553f9e
Christopher Lee <99973434+topherlee@users.noreply.github.com>
2023-09-14 19:31:53 +0100

    Use pinchable to zoom on home and post details

--- src/components/atoms/AstroImage/AstroImage.js
+++ src/components/atoms/AstroImage/AstroImage.js
@@ -1,17 +1,19 @@
-import React from "react";
-import {
-  Dimensions
-} from 'react-native';
+import React from 'react';
+import {Dimensions} from 'react-native';
+import Pinchable from 'react-native-pinchable';
 
-import AutoscaleImage from "../AutoscaleImage/AutoscaleImage";
+import AutoscaleImage from '../AutoscaleImage/AutoscaleImage';
 
+const AstroImageWrapper = props => {
+    //console.log(props);
 
-const AstroImageWrapper = (props) => {
-  //console.log(props);
-
-  return (
-      <AutoscaleImage uri={props.thumbnail} width={(0.96 * Dimensions.get('window').width - 33) / 2} aspectRatio={props.aspectRatio} />
-  );
+    return (
+        <AutoscaleImage
+            uri={props.thumbnail}
+            width={(0.96 * Dimensions.get('window').width - 33) / 2}
+            aspectRatio={props.aspectRatio}
+        />
+    );
 };
 
 export default AstroImageWrapper;
--- src/components/atoms/AutoscaleImage/AutoscaleImage.js
+++ src/components/atoms/AutoscaleImage/AutoscaleImage.js
@@ -1,25 +1,29 @@
-import React, { Component } from "react";
-import { Image } from "react-native";
-import { PropTypes } from "prop-types"
-import FastImage from "react-native-fast-image";
-
+import React, {Component} from 'react';
+import {Image} from 'react-native';
+import {PropTypes} from 'prop-types';
+import FastImage from 'react-native-fast-image';
+import Pinchable from 'react-native-pinchable';
 export default class ScaledImage extends Component {
     constructor(props) {
         super(props);
-        this.state = { source: { uri: this.props.uri } };
+        this.state = {source: {uri: this.props.uri}};
     }
 
     render() {
         return (
-            <FastImage
-                source={this.state.source}
-                style={{
-                    height: this.props.height,
-                    width: this.props.width,
-                    aspectRatio: this.props.aspectRatio ? this.props.aspectRatio : 1,
-                }}
-                resizeMethod="scale"
-            />
+            <Pinchable maximumZoomScale={10}>
+                <FastImage
+                    source={this.state.source}
+                    style={{
+                        height: this.props.height,
+                        width: this.props.width,
+                        aspectRatio: this.props.aspectRatio
+                            ? this.props.aspectRatio
+                            : 1,
+                    }}
+                    resizeMethod="scale"
+                />
+            </Pinchable>
         );
     }
 }
@@ -27,5 +31,5 @@ export default class ScaledImage extends Component {
 ScaledImage.propTypes = {
     uri: PropTypes.string.isRequired,
     width: PropTypes.number,
-    height: PropTypes.number
+    height: PropTypes.number,
 };
