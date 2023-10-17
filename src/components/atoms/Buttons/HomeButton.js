a19c49a6e6a738f0b24fca088ca3c257d34f01b3
nutcracker22 <nutcracker@mailbox.org>
2023-10-17 21:10:48 +0100

    changed home button

--- src/components/atoms/Buttons/HomeButton.js
+++ src/components/atoms/Buttons/HomeButton.js
@@ -2,23 +2,29 @@ import React from "react";
 import {
     View,
     Image,
+    Text
 } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 
 import globalStyling from "../../../../constants/globalStyling";
 import HomeButtonSvg from '../../../assets/buttons/home-button.svg';
 
-const HomeButton = () => (
+const HomeButton = (focused) => (
+    //focused ? (let --NavBarButtonBackground = '#333') : 'unfocused'}
+    
     <View style={{
         alignItems: 'center',
         justifyContent: 'center',
         top: 0
     }}>
+
         <HomeButtonSvg style={globalStyling.NavBarButton}/>
         {/* <Image
             source={require('../../../assets/buttons/home-button.png')}
             resizeMode="contain"
             style={globalStyling.NavBarButton}
         /> */}
+        {focused ? <Text style={globalStyling.SortByModalTextGrey}>focused</Text> : <Text style={globalStyling.SortByModalTextGrey}>not focused</Text>}
     </View>
 );
 
