3446eb2d829cfdeb75ff48eac1379a6608e69969
nutcracker22 <nutcracker@mailbox.org>
2024-02-08 20:45:11 +0000

    applied old formatting to make comparison easier

--- src/screens/SigninScreen/LoginScreen.js
+++ src/screens/SigninScreen/LoginScreen.js
@@ -1,154 +1,154 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
 } from 'react-native';
 import * as Keychain from 'react-native-keychain';
-import { AuthContext } from '../../context/AuthContext';
+import {AuthContext} from '../../context/AuthContext';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import globalStyling from "../../../constants/globalStyling";
-import { loadCurrentUser } from "../../utils";
+import globalStyling from '../../../constants/globalStyling';
+import { loadCurrentUser } from '../../utils';
 
 export default function LoginScreen({ navigation, route }) {
-  const {
-    setIsSignedIn,
-    domain,
-    resetStates,
-    setToken,
-    fetchInstance,
-    setValidSubscription
-  } = useContext(AuthContext);     //get setIsSignedIn function from global context
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [error, setError] = useState(false);
-  const [securePassword, setSecurePassword] = useState(true);
-  const passwordInput = useRef(null);
+    const {
+      setIsSignedIn,
+      domain,
+      resetStates,
+      setToken,
+      fetchInstance,
+      setValidSubscription
+    } = useContext(AuthContext);     //get setIsSignedIn function from global context
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState(false);
+    const [securePassword, setSecurePassword] = useState(true);
+    const passwordInput = useRef(null);
 
 
-  async function handleLogin() {
+    async function handleLogin() {
 
-    var body = JSON.stringify({
-      'username': username,
-      'password': password
-    })
+        var body = JSON.stringify({
+          'username': username,
+          'password': password
+        })
 
-    await fetch(`${domain}/api/auth/login/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: body
-    })
-      .then(async (res) => {
-        console.log('STATUS', res.status);
-        if (res.ok) {
-          return res.json();
-        } else {
-          setError(true)
-          console.log('error')
-          throw await res.json();
-        }
-      })
-      .then(async json => {
-        console.debug('JSON', json);
-        await Keychain.setGenericPassword('token', JSON.stringify(json))
-        setToken(json);
-        setIsSignedIn(true);
+        await fetch(`${domain}/api/auth/login/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: body
+        })
+            .then(async (res) => {
+                console.log('STATUS', res.status);
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    setError(true)
+                    console.log('error')
+                    throw await res.json();
+                }
+            })
+            .then(async json => {
+                console.debug('JSON', json);
+                await Keychain.setGenericPassword('token', JSON.stringify(json))
+                setToken(json);
+                setIsSignedIn(true);
 
-        const curUser = await loadCurrentUser(json, fetchInstance)
-        setValidSubscription(curUser.valid_subscription)
-        console.debug("validSubscription", curUser.valid_subscription);
-      })
-      .catch(error => {
-        console.log("LoginScreen error", error);
-        setError(true)
-      })
-  }
+                const curUser = await loadCurrentUser(json, fetchInstance)
+                setValidSubscription(curUser.valid_subscription)
+                console.debug("validSubscription", curUser.valid_subscription);
+            })
+            .catch(error => {
+                console.log("LoginScreen error", error);
+                setError(true)
+            })
+      }
 
-  useEffect(() => {
-    resetStates();
-  }, []);
+    useEffect(() => {
+        resetStates();
+    }, []);
 
 
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}>
-      <Image
-        style={styles.image}
-        resizeMode="contain"
-        source={require('../../assets/logo-text-gray.png')}
-      />
-      {error ? (
-        <Text style={styles.titleRed}>
-          Error logging in. Please try again
-        </Text>
-      ) : (
-        <Text style={styles.title}>Register or Login</Text>
-      )}
-      <View
-        keyboardShouldPersistTaps='handled'
-        style={globalStyling.inputView}>
-        <TextInput
-          style={globalStyling.inputFieldText}
-          textContentType={'username'}
-          placeholder="Username"
-          placeholderTextColor="grey"
-          autoCapitalize={'none'}
-          autoCorrect={false}
-          clearButtonMode="while-editing"
-          returnKeyType="next"
-          defaultValue={route.params?.username && route.params.username}
-          onChangeText={(username) => setUsername(username)}
-          onBlur={() => setError(false)}
-          blurOnSubmit={false}
-          onSubmitEditing={() => {
-            passwordInput.current.focus();
-          }}
-        />
-      </View>
-      <View style={globalStyling.inputView}>
-        <TextInput
-          style={globalStyling.inputFieldText}
-          textContentType={'password'}
-          autoCapitalize={'none'}
-          autoCorrect={false}
-          placeholder="Password"
-          placeholderTextColor="grey"
-          secureTextEntry={securePassword}
-          onChangeText={(password) => setPassword(password)}
-          onSubmitEditing={handleLogin}
-          onBlur={() => setError(false)}
-          ref={passwordInput}
-        />
-        <TouchableOpacity style={{ position: "absolute", right: 1 }} onPress={() => setSecurePassword(!securePassword)}>
-          <Icon name={securePassword ? "eye-outline" : "eye-off-outline"} size={30} color="lightgray" />
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity onPress={function () { navigation.navigate('ForgotPassword') }}>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.loginBtn}
-        onPress={() => handleLogin()}
-        testID="loginButton"
-      >
-        <Text style={styles.loginText}>LET'S GO</Text>
-      </TouchableOpacity>
-      <View style={styles.bottomText}>
-        <Text style={styles.text}>Don't have an account? </Text>
-        <TouchableOpacity onPress={function () { navigation.navigate('SignUp') }}>
-          <Text style={{ color: "#FFC700" }}> Register here</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  );
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}>
+            <Image
+                style={styles.image}
+                resizeMode="contain"
+                source={require('../../assets/logo-text-gray.png')}
+            />
+            {error ? (
+                <Text style={styles.titleRed}>
+                  Error logging in. Please try again
+                </Text>
+            ) : (
+                <Text style={styles.title}>Register or Login</Text>
+            )}
+            <View
+                keyboardShouldPersistTaps='handled'
+                style={globalStyling.inputView}>
+                <TextInput
+                    style={globalStyling.inputFieldText}
+                    textContentType={'username'}
+                    placeholder="Username"
+                    placeholderTextColor="grey"
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    clearButtonMode="while-editing"
+                    returnKeyType="next"
+                    defaultValue={route.params?.username && route.params.username}
+                    onChangeText={(username) => setUsername(username)}
+                    onBlur={() => setError(false)}
+                    blurOnSubmit={false}
+                    onSubmitEditing={() => {
+                        passwordInput.current.focus();
+                    }}
+                />
+            </View>
+            <View style={globalStyling.inputView}>
+                <TextInput
+                    style={globalStyling.inputFieldText}
+                    textContentType={'password'}
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    placeholder="Password"
+                    placeholderTextColor="grey"
+                    secureTextEntry={securePassword}
+                    onChangeText={(password) => setPassword(password)}
+                    onSubmitEditing={handleLogin}
+                    onBlur={() => setError(false)}
+                    ref={passwordInput}
+                />
+                <TouchableOpacity style={{ position: "absolute", right: 1 }} onPress={() => setSecurePassword(!securePassword)}>
+                    <Icon name={securePassword ? "eye-outline" : "eye-off-outline"} size={30} color="lightgray" />
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity onPress={function () { navigation.navigate('ForgotPassword') }}>
+                <Text style={styles.forgot_button}>Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.loginBtn}
+                onPress={() => handleLogin()}
+                testID="loginButton"
+            >
+                <Text style={styles.loginText}>LET'S GO</Text>
+            </TouchableOpacity>
+            <View style={styles.bottomText}>
+                <Text style={styles.text}>Don't have an account? </Text>
+                <TouchableOpacity onPress={function () { navigation.navigate('SignUp') }}>
+                    <Text style={{ color: "#FFC700" }}> Register here</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
