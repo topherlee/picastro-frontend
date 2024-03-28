be99cadbd6efc5eb2c7b9f70ddb07604c528ce35
Christopher Lee <topher.lee10@gmail.com>
2024-03-28 01:09:12 +0000

    Cleanup unused packages

--- src/components/molecules/ImageContainers/FullWidthImage.js
+++ src/components/molecules/ImageContainers/FullWidthImage.js
@@ -1,20 +1,17 @@
 import {View, Dimensions} from 'react-native';
 import React from 'react';
 import {AutoscaleImageIndicator} from '../../atoms';
-import Pinchable from 'react-native-pinchable';
 
 const FullWidthImage = ({props}) => {
 	// console.log('DFI', props);
 
 	return (
 		<View>
-			{/* <Pinchable maximumZoomScale={10}> */}
 			<AutoscaleImageIndicator
 				uri={props.image}
 				width={0.93 * Dimensions.get('window').width}
 				aspectRatio={props.aspectRatio}
 			/>
-			{/* </Pinchable> */}
 		</View>
 	);
 };
